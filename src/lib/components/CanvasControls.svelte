8a9255ddd5625333dd0b87d2111d382c9139d64a
akolt19d <a.kolt.19d@gmail.com>
2025-04-18 14:50:58 +0200

    Add reactive zoom

--- src/lib/components/CanvasControls.svelte
+++ src/lib/components/CanvasControls.svelte
@@ -1,12 +1,28 @@
+<script lang="ts">
+    import { increaseZoom, decreaseZoom, isMinZoom, isMaxZoom } from "$lib/stores/Zoom.svelte";
+
+    let isMax = $state(isMaxZoom())
+    let isMin = $state(isMinZoom())
+
+    $effect(() => {
+        isMax = isMaxZoom()
+        isMin = isMinZoom()
+    })
+</script>
+
 <div class="absolute bottom-3 left-3 p-2 rounded z-10 flex items-center justify-center bg-black/30">
   <div class="flex flex-col items-center gap-2">
     <button
-      class="rounded bg-white px-3 aspect-square text-black transition hover:bg-gray-200"
+        class="rounded bg-white px-3 aspect-square text-black transition hover:bg-gray-200 disabled:brightness-50"
+        onclick={increaseZoom}
+        disabled={isMax}
     >
       +
     </button>
     <button
-      class="rounded bg-white px-3 aspect-square text-black transition hover:bg-gray-200"
+        class="rounded bg-white px-3 aspect-square text-black transition hover:bg-gray-200 disabled:brightness-50"
+        onclick={decreaseZoom}
+        disabled={isMin}
     >
       -
     </button>
