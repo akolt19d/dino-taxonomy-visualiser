cb3881cc84de03814296e07941019058ec8b2f0c
akolt19d <a.kolt.19d@gmail.com>
2025-06-08 00:03:32 +0200

    Expand DinoInfo component

--- src/lib/components/DinoInfo.svelte
+++ src/lib/components/DinoInfo.svelte
@@ -18,17 +18,51 @@
 </script>
 
 {#if dinosaur}
-    <div id="dino-info-wrapper" class="absolute h-full top-0 right-0 p-2 overflow-hidden">
-        <div transition:fly={{ x: 200, duration: 500 }} class="relative px-8 py-4 w-full h-full bg-neutral-900 border border-neutral-800 rounded-2xl">
-            <button class="absolute right-4 top-1 text-xl cursor-pointer" onclick={handleClose}>X</button>
+    <div id="dino-info-wrapper" class="absolute top-0 right-0 p-2 overflow-hidden">
+        <div transition:fly={{ x: 200, duration: 500 }} class="relative px-8 py-8 w-full h-full bg-neutral-900 border border-neutral-800 rounded-2xl">
+            <button class="absolute right-4 top-2 text-xl cursor-pointer" onclick={handleClose}>X</button>
             <h1 class="italic text-2xl text-center">
                 {dinosaur.name} {dinosaur.species}
             </h1>
-            <img src={dinosaur.imgLink} alt={dinosaur.name} class="my-4">
-            <ul>
-                <li>diet: {dinosaur.diet}</li>
-                <li>lived {dinosaur.period}</li>
-            </ul>
+            <img src={dinosaur.imgLink} alt={dinosaur.name} class="mb-8 mt-6 mx-auto">
+            <table class="w-full text-center mb-4">
+                <tbody>
+                    <tr>
+                        <th>Type</th>
+                        <td>{dinosaur.type}</td>
+                    </tr>
+                    <tr>
+                        <th>Diet</th>
+                        <td>{dinosaur.diet}</td>
+                    </tr>
+                    <tr>
+                        <th>Length</th>
+                        <td>{dinosaur.length}</td>
+                    </tr>
+                    <tr>
+                        <th>Time period</th>
+                        <td>{dinosaur.period}</td>
+                    </tr>
+                    <tr>
+                        <th>Lived in</th>
+                        <td>{dinosaur.livedIn}</td>
+                    </tr>
+                    <tr>
+                        <th>Named by</th>
+                        <td>{dinosaur.namedBy}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <span>
+                Reference: 
+                <a href={dinosaur.link} class=" text-sky-600 underline">{dinosaur.link}</a>
+            </span>
         </div>
     </div>
 {/if}
+
+<style>
+    th, td {
+        border: 1px solid white;
+    }
+</style>
